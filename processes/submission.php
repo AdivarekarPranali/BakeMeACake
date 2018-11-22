32ce43de381223f9db6c8c278a17f0b02ae8288b
anshuranisingh <singhanshurani1995@gmail.com>
2018-11-22 16:48:13 +0530

    Delete submission.php

--- processes/submission.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../includes/initialize.php');
-session_start();
-$data_for = mysqli_real_escape_string($con, $_POST['data_for']);
-$_SESSION[$data_for] = mysqli_real_escape_string($con, $_POST['data']);
-
-$jsonarray = array("error" => 0);
-echo json_encode($jsonarray); 
-?>
